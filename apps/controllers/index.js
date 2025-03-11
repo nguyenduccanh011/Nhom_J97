c4682fe79ac47b52a07653acf8b7a387939da99b
Tran Lieu Huy Khanh <lieuhuykhanh@gmail.com>
2025-03-11 08:47:28 +0700

    Adjust

--- apps/controllers/index.js
+++ apps/controllers/index.js
@@ -1,9 +1,8 @@
 var express = require("express");
 var router = express.Router();
-router.use("/home", require(__dirname + "/homecontroller"));
 // router.use("/product", require(__dirname + "/productcontroller"));
 router.get("/", function (req, res) {
-  res.render("home.ejs");
+  res.render("index.ejs");
 });
 module.exports = router;
 // router.use("/admin", require(__dirname + "/admin/admincontroller"));
