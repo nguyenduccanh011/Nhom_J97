dc2cc7fd6c7e3de679c134b224f8451ead5798f9
Le Cong Chuc <0l14l301@gmail.com>
2025-03-11 10:12:48 +0700

    api in home

--- apps/controllers/apicontroller.js
+++ apps/controllers/apicontroller.js
@@ -1,12 +1,45 @@
 var express = require("express");
 const { ObjectId } = require("mongodb");
 var router = express.Router();
-var Product = require("./../entity/product");
+var Example = require("../entity/example");
+var ExampleService = require("./../services/exampleService");
+var Product = require("../entity/product");
 var ProductService = require("./../services/productService");
 
-// router.use("/", function (req, res) {
-//   res.render("product.ejs");
-// });
+router.get("/example-list", async function (req, res) {
+  var exampleService = new ExampleService();
+  var productList = await exampleService.getProductList();
+  res.json(productList);
+});
+router.get("/get-example", async function (req, res) {
+  var exampleService = new ExampleService();
+  var product = await exampleService.getProduct(req.query.id);
+  res.json(product);
+});
+router.post("/insert-example", async function (req, res) {
+  // console.log(req.body);
+  var exampleService = new ExampleService();
+  var product = new Example();
+  product.Name = req.body.Name;
+  product.Price = req.body.Price;
+  var result = await exampleService.insertProduct(product);
+  res.json({ status: true, message: result });
+});
+router.post("/update-example", async function (req, res) {
+  var exampleService = new ExampleService();
+  var product = new Example();
+  product._id = new ObjectId(req.body._id);
+  product.Name = req.body.Name;
+  product.Price = req.body.Price;
+  var result = await exampleService.updateProduct(product);
+  res.json({ status: true, message: result });
+});
+router.delete("/delete-example", async function (req, res) {
+  var exampleService = new ExampleService();
+  var result = await exampleService.deleteProduct(req.query.id);
+  res.json({ status: true, message: result });
+});
+
 router.get("/product-list", async function (req, res) {
   var productService = new ProductService();
   var productList = await productService.getProductList();
@@ -23,6 +56,9 @@ router.post("/insert-product", async function (req, res) {
   var product = new Product();
   product.Name = req.body.Name;
   product.Price = req.body.Price;
+  product.link_img = req.body.link_img;
+  product.Description = req.body.Description;
+  product.Weight = req.body.Weight;
   var result = await productService.insertProduct(product);
   res.json({ status: true, message: result });
 });
@@ -32,6 +68,9 @@ router.post("/update-product", async function (req, res) {
   product._id = new ObjectId(req.body._id);
   product.Name = req.body.Name;
   product.Price = req.body.Price;
+  product.link_img = req.body.link_img;
+  product.Description = req.body.Description;
+  product.Weight = req.body.Weight;
   var result = await productService.updateProduct(product);
   res.json({ status: true, message: result });
 });
--- apps/controllers/index.js
+++ apps/controllers/index.js
@@ -1,8 +1,21 @@
 var express = require("express");
 var router = express.Router();
+var ProductService = require("./../services/productService");
+var productService = new ProductService();
+
 // router.use("/product", require(__dirname + "/productcontroller"));
-router.get("/", function (req, res) {
-  res.render("home.ejs");
+router.get("/", async function (req, res) {
+  try {
+    var productList = await productService.getProductList();
+    for (var i = 0; i < productList.length; i++) {
+      productList[i].Price = productList[i].Price.$numberInt;
+      productList[i].Weight = productList[i].Weight.$numberInt;
+    }
+    res.render("homeapi.ejs", { products: productList });
+  } catch (e) {
+    console.error(err);
+    res.status(500).send("Lỗi máy chủ");
+  }
 });
 router.use("/api", require(__dirname + "/apicontroller"));
 
